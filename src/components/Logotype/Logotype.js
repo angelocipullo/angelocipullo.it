3e4e2ceb9d68d6a41ea54b50d904393f0e14b4b7
Angelo Cipullo <angelo.cipullo@outlook.it>
2023-01-21 18:37:34 +0100

    adjusted logo subtitle

--- src/components/Logotype/Logotype.js
+++ src/components/Logotype/Logotype.js
@@ -6,7 +6,7 @@ const Logotype = (props) => {
     return (
         <div className='mx-auto'>
             <h1 className='text-center font-primary font-[900] tracking-tight text-5xl lg:text-7xl'>Angelo Cipullo</h1>
-            <p className='text-center text-xl lg:text-2xl text-gray-500 mt-2 font-light'>Front End Developer — React Developer</p>
+            <p className='text-center text-xl lg:text-2xl text-gray-500 mt-2 font-light'>Web Developer</p>
         </div>
     )
 }
